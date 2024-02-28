750e821296fa9af39500994a3222ad1b74e4c4c4
huanyu <2024627697@qq.com>
2024-02-28 17:58:09 +0800

    新增菜品业务代码开发

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,10 +1,26 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
+import com.sky.entity.Dish;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface DishMapper {
+
+    /**
+     * 根据分类id查询菜品数据
+     * @param id
+     * @return
+     */
     @Select("select count(*) from dish where category_id = #{id}")
     Integer countByCategoryId(Long id);
+
+    /**
+     * 插入菜品数据
+     * @param dish
+     */
+    @AutoFill(value = OperationType.INSERT)
+    void insert(Dish dish);
 }
